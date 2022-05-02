271813b1c354716319fe23cf4f3689496b054c06
Toon239 <va.qneva@gmail.com>
2022-05-03 00:22:41 +0300

    Added simple command interface

--- main.cpp
+++ main.cpp
@@ -1,22 +1,69 @@
 #include "library.h"
 #include <iostream>
 #include <fstream>
+#include "usefulFunctions.h"
 using namespace std;
+#define MAX_LENGTH 1024
 int main()
 {
-	Book b1("Agatha Christie","ABC murders","abcmurders.txt","A short mystery novel");
-	/*Book b2("Fredrik Backman", "A man called Ove", "Ove.txt", "A heartwarming story about an old man and his life");
-	Book b3("Mikhail Bugakov", "Master and Margarita", "Margarita.txt", "A glimpse into the everyday life of russian folk, the devil and his companions");
-	Book b4("Jonas Jonasson", "The girl who saved the king of Sweden", "Nombeko.txt", "The story of a Nigerian girl who gets out of the slumps");
-	Book b5("Thomas Mann", "The Buddenbrooks", "Buddenbrooks.txt", "The slow demise of a once rich and well-respected family");
-	Library lib;
-	lib.addBook(b1);
-	lib.addBook(b2);
-	lib.addBook(b3);
-	lib.addBook(b4);
-	lib.addBook(b5);
-	lib.Sort();*/
-	ofstream out(b1.getFileName());
-	out << b1;
-	return 0;
+	const char* adminPassword = "IDK123!";
+	char input[MAX_LENGTH];
+	char* command;
+	cout << "Enter \"help\" for list of commands\n";
+	Library library;
+	do
+	{
+		cin.getline(input, MAX_LENGTH);
+		int length = strlen(input) + 1;
+		command = new char[length];
+		strcpy(command, input);
+		if (strcmp(command, "help") == 0)
+		{
+			cout << "1.sort\n2.find\n3.add\n4.remove\n5.display\n6.exit\n";
+		}
+		else if (strcmp(command, "sort") == 0)
+		{
+			library.sort();
+			delete[] command;
+		}
+		else if (strcmp(command, "find") == 0)
+		{
+			library.find();
+			delete[] command;
+		}
+		else if (strcmp(command, "add") == 0)
+		{
+			if (authorize(adminPassword))
+			{
+				Book book;
+				setField(book, "Title", &(Book::setTitle));
+				setField(book, "Author", &(Book::setAuthor));
+				setField(book, "Description", &(Book::setDescription));
+				setField(book, "File Name", &(Book::setFileName));
+				//Rating
+				cout << "Enter Rating: 1-5.";
+				int rating;
+				cin >> rating;
+				book.setRating(rating);
+				library.addBook(book);
+			}
+			delete[] command;
+		}
+		else if (strcmp(command, "remove") == 0)
+		{
+			if (authorize(adminPassword))
+			{
+				library.removeBook(library.findBy());
+			}
+			delete[] command;
+		}
+		else if (strcmp(command, "display") == 0)
+		{
+			library.displayBook(library.findBy());
+			delete[] command;
+		}
+		
+	} while (strcmp(command, "exit") != 0);
 }
+
+
